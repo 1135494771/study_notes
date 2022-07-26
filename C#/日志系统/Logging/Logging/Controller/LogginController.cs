72cab83c279a88a0a466a03237b5375b274b03bc
chengquanxi <18101381410@163.com>
2022-07-26 18:04:02 +0800

    添加日志配置文件

---
+++ C#/日志系统/Logging/Logging/Controller/LogginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging.Controller
+{
+    internal class LogginController
+    {
+        //一般默认 ILogger<当前类>
+        private readonly ILogger<LogginController> logger;
+
+        //使用构造函数方式注入 ILogger对象
+        public LogginController(ILogger<LogginController> _logger)
+        {
+            this.logger = _logger;
+        }
+
+        public void RecordLog()
+        {
+            logger.LogDebug("调试日志");
+            logger.LogError("错误日志");
+            logger.LogInformation("信息日志");
+            logger.LogWarning("警告日志");
+        }
+
+    }
+}
---
+++ C#/日志系统/Logging/Logging/Program.cs
@@ -0,0 +1,30 @@
+﻿using Logging.Controller;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建DI ServiceCollection对象
+            ServiceCollection services = new ServiceCollection();
+            services.AddLogging(logBuilder => { 
+                //输出到控制台
+                logBuilder.AddConsole();
+                //设置文件最小输出等级
+                logBuilder.SetMinimumLevel(LogLevel.Debug); 
+            });
+            //注入LogginController 服务对象
+            services.AddScoped<LogginController>();
+            using (ServiceProvider provider = services.BuildServiceProvider())
+            {
+                var temp = provider.GetRequiredService<LogginController>();
+                //执行服务对象方法
+                temp.RecordLog();
+            }
+        }
+    }
+}
