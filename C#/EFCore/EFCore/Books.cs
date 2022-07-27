7d729cf2363d2b83eea4e3d5c13f8f9722b0c41d
程泉喜 <18101381410@163.com>
2022-07-27 23:56:17 +0800

    s

---
+++ C#/EFCore/EFCore/Books.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class Books
+    {
+        //主键
+        public long Id { get; set; }
+
+        //标题
+        public string Title { get; set; }
+
+        //发布日期
+        public DateTime PubTime { get; set; }
+
+        //单价
+        public double Price { get; set; }
+    }
+}
