705043eb1cd32eeed059789b34a2a1861cdd4159
chengquanxi <18101381410@163.com>
2022-08-16 18:12:52 +0800

    s

---
+++ C#/.NetCore入门/NetCoreWebApplication/NetCoreWebApplication/Controllers/LoginsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebApplication.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        [HttpPost("{id}")]
+        public ActionResult<LoginResult> Login(int id, LoginRequest loginReq)
+        {
+            if (loginReq.UserName == "admin" && loginReq.Password == "123456")
+            {
+                return new LoginResult(true, "登录成功");
+            }
+            else
+            {
+                return new LoginResult(false, "登录失败");
+            }
+        }
+
+        public record LoginRequest(string UserName, string Password);
+
+        public record LoginResult(bool result, string data);
+
+    }
+}
